5de6025795e50a7ffef7e45cf27aa5bb14cfb3ca
Erove13 <ema.rosas.v@gmail.com>
2025-03-02 20:59:39 +0100

    change of plans

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
 MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
 
-# Verifica si las variables de entorno están cargadas correctamente
-print(f'MONGO_URI: {os.getenv("MONGO_URI")}')
-print(f'MONGO_DB: {os.getenv("MONGO_DB")}')
-print(f'MONGO_COLLECTION: {os.getenv("MONGO_COLLECTION")}')
+# Verifica si las variables se cargan correctamente
+print(f"MONGO_URI: {MONGO_URI}")
+print(f"MONGO_DB: {MONGO_DB}")
+print(f"MONGO_COLLECTION: {MONGO_COLLECTION}")
 
-# Verificar que las variables no están vacías
+# Verifica que las variables no estén vacías
 if not MONGO_URI or not MONGO_DB or not MONGO_COLLECTION:
     raise ValueError("Faltan algunas variables de entorno en el archivo .env")
 
@@ -23,9 +23,15 @@ if not MONGO_URI or not MONGO_DB or not MONGO_COLLECTION:
 if not isinstance(MONGO_DB, str):
     raise TypeError("MONGO_DB debe ser una cadena de texto")
 
-# Crear conexión a MongoDB
-client = MongoClient(MONGO_URI)
+try:
+    # Crear conexión a MongoDB
+    client = MongoClient(MONGO_URI)
 
-# Acceder a la base de datos y colección
-db = client[MONGO_DB]  # Esto debe ser una cadena (str)
-collection = db[MONGO_COLLECTION]
+    # Acceder a la base de datos y colección
+    db = client[MONGO_DB]  # Esto debe ser una cadena (str)
+    collection = db[MONGO_COLLECTION]
+
+    print("Conexión exitosa a MongoDB")
+
+except Exception as e:
+    print(f"Error de conexión a MongoDB: {e}")
